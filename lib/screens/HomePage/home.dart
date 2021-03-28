e4b647bfe7a03dc4ec5d492e0d6f440275bbabcd
excdk <yancovvictor@gmail.com>
2021-03-28 11:52:53 +0500

    добавил Класс утилей для картинок - асинхронный метод побайтовой считки ПНГ (для маркеров гугл карты), обновил зависимости пакета гугл карты, добавил пакет location

--- lib/screens/HomePage/home.dart
+++ lib/screens/HomePage/home.dart
@@ -1,10 +1,10 @@
+import 'dart:collection';
 import 'dart:typed_data';
 import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'dart:async';
+import 'package:recycler_flutter_app/utils/ImagesUtils.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Home extends StatefulWidget {
@@ -13,37 +13,34 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  GoogleMapController _mapController;
   static final LatLng _tyumenCenter = LatLng(57.151327, 65.53716);
   LatLng _lastMapPos = _tyumenCenter;
-  final Set<Marker> _markers = {};
+
+  GoogleMapController _mapController;
+  Set<Marker> _markers = HashSet<Marker>();
   BitmapDescriptor _markerIcon;
 
   @override
   void initState() {
     super.initState();
-    getBytesFromAsset('assets/images/009-recycle.png', 64).then((onValue) {
-      _markerIcon = BitmapDescriptor.fromBytes(onValue);
-    });
+    _setMarkerIcon();
   }
 
   void _onMapCreated(GoogleMapController controller) {
     _mapController = controller;
     setState(() {
-      _markers.add(
-        Marker(
-            markerId: MarkerId("00000"),
-            position: _lastMapPos,
-            infoWindow: InfoWindow(title: "TITLE", snippet: "Snippet"),
-            icon: _markerIcon),
-      );
+      _markers.add(Marker(
+          markerId: MarkerId("0"),
+          position: _lastMapPos,
+          infoWindow: InfoWindow(title: "TITLE", snippet: "Snippet"),
+          icon: _markerIcon));
     });
   }
 
   void _setMarkerIcon() async {
-    _markerIcon = await BitmapDescriptor.fromAssetImage(
-        ImageConfiguration(size: Size(100, 100)),
-        'assets/images/009-recycle.png');
+    _markerIcon = BitmapDescriptor.fromBytes(
+        await ImagesUtils.getBytesFromPngAsset(
+            'lib/assets/images/markers/009-recycle.png', 100));
   }
 
   @override
@@ -56,15 +53,5 @@ class _HomeState extends State<Home> {
         markers: _markers,
       ),
     );
-  }
-
-  static Future<Uint8List> getBytesFromAsset(String path, int width) async {
-    ByteData data = await rootBundle.load(path);
-    Codec codec = await instantiateImageCodec(data.buffer.asUint8List(),
-        targetWidth: width);
-    FrameInfo fi = await codec.getNextFrame();
-    return (await fi.image.toByteData(format: ImageByteFormat.png))
-        .buffer
-        .asUint8List();
   }
 }
